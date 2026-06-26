5f518abb0fdc8482b97819faa3cadc420c4b15b0
Bruno Cruvinel <240232549+brunostigmata-coder@users.noreply.github.com>
2026-06-26 10:53:00 -0300

    XKORE 0
    XKORE 0

--- src/Network/Send/ROla.pm
+++ src/Network/Send/ROla.pm
@@ -9,16 +9,26 @@ sub new {
 	my ( $class ) = @_;
 	my $self = $class->SUPER::new( @_ );
 
+	# ReClassic (cliente RagexeRE 2020-03-04) usa o fluxo de login do kRO Zero,
+	# nao o 0C26/0825 do ROla "oficial". Capturado da DLL/cliente:
+	#   0x0ACF master_login (senha Rijndael 32B, game_code "0011", flag "G000")
+	#   -> 0x0AE3 received_login_token (login_type=0)
+	#   -> 0x0064 token_login (SENHA EM TEXTO PLANO, formato classico V Z24 Z24 C)
+	# So afeta XKore 0/2; no XKore 1 quem loga e o cliente real.
 	my %packets = (
-		'0C26' => [ 'master_login', 'a4 Z51 a32 a5',        [qw(game_code username password_rijndael flag)] ],
-		'0825' => [ 'token_login',  'v V C Z51 a17 a15 a*', [qw(len version master_version username mac_hyphen_separated ip token)] ],
-		'0436' => [ 'map_login',    'a4 a4 a4 V V C',       [qw(accountID charID sessionID unknown tick sex)] ],
+		'0ACF' => [ 'master_login', 'a4 Z25 a32 a5', [qw(game_code username password_rijndael flag)] ],
+		'0064' => [ 'token_login',  'V Z24 Z24 C',   [qw(version username password master_version)] ],
+		'0436' => [ 'map_login',    'a4 a4 a4 V C',   [qw(accountID charID sessionID tick sex)] ],
+		# ReClassic usa 0x00F3 pra public_chat (o ServerType0 manda 0x008C, errado
+		# aqui -> o servidor derruba). 'v a*' = len + msg SEM null, igual ao cliente
+		# real (captura: f3 00 <len> <msg 32B> = 36 bytes).
+		'00F3' => [ 'public_chat',  'v a*',          [qw(len message)] ],
 	);
 
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
 
 	my %handlers = qw(
-		token_login 0825
+		token_login 0064
 		actor_action 0437
 		actor_info_request 0368
 		actor_look_at 0361
@@ -48,7 +58,8 @@ sub new {
 		storage_item_remove 0365
 		storage_password 023B
 		sync 0360
-		master_login 0C26
+		public_chat 00F3
+		master_login 0ACF
 		rodex_open_mailbox 0AC0
 		rodex_refresh_maillist 0AC1
 	);
@@ -71,52 +82,50 @@ sub new {
 	return $self;
 }
 
-sub reconstruct_master_login {
-	my ( $self, $args ) = @_;
+# 1o passo do login (XKore 0/2): manda 0x0ACF com a senha cifrada em Rijndael
+# de 32 bytes, igual ao cliente kRO Zero. game_code "0011" e flag "G000" sao os
+# valores capturados do cliente ReClassic.
+sub sendMasterLogin {
+	my ( $self, $username, $password, $master_version, $version ) = @_;
 
-	if ( exists $args->{password} ) {
-		for ( Digest::MD5->new ) {
-			$_->add( $args->{password} );
-			$args->{password_md5}     = $_->clone->digest;
-			$args->{password_md5_hex} = $_->hexdigest;
-		}
-
-		my $key      = pack( 'C32', ( 0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06, 0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06 ) );
-		my $chain    = pack( 'C32', ( 0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41, 0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41 ) );
-		my $in       = pack( 'a32', $args->{password} );
-		my $rijndael = Utils::Rijndael->new;
-		$rijndael->MakeKey( $key, $chain, 32, 32 );
-		$args->{password_rijndael} = $rijndael->Encrypt( $in, undef, 32, 0 );
-	}
-}
-
-sub sendTokenToServer {
-	my ( $self, $username, $password, $master_version, $version, $token, $length, $ip, $port ) = @_;
-	my $len = $length + 92;
-
-	$net->serverDisconnect();
-	$net->serverConnect( $ip, $port );
-
-	my $ip                   = sprintf("192.168.%02d.%02d", (map { int(rand(255)) } 1..2));
-	my $mac                  = $config{macAddress} || sprintf("E0311E%02X%02X%02X", (map { int(rand(256)) } 1..3));
-	my $mac_hyphen_separated = join '-', $mac =~ /(..)/g;
+	my $password_rijndael = $self->encrypt_password( $password );
 
 	my $msg = $self->reconstruct(
 		{
-			switch               => 'token_login',
-			len                  => $len,
-			version              => $version || $self->version,
-			master_version       => $master_version,
-			username             => $username,
-			mac_hyphen_separated => $mac_hyphen_separated,
-			ip                   => $ip,
-			token                => $token,
+			switch            => 'master_login',
+			game_code         => '0011',
+			username          => $username,
+			password_rijndael => $password_rijndael,
+			flag              => 'G000',
 		}
 	);
 
 	$self->sendToServer( $msg );
+	debug "Sent ReClassic master_login (0ACF)\n", "sendPacket", 2;
+}
 
-	debug "Sent sendTokenLogin\n", "sendPacket", 2;
+# 2o passo: depois do 0x0AE3 (token, login_type=0), reconecta no login server e
+# manda 0x0064 com a SENHA EM TEXTO PLANO (formato classico V Z24 Z24 C).
+# version=55 e master_version=22 sao os bytes que o cliente real envia (0x37/0x16);
+# o master_version do servers.txt (=1) e so controle interno do OpenKore.
+sub sendTokenToServer {
+	my ( $self, $username, $password, $master_version, $version, $token, $length, $ip, $port ) = @_;
+
+	$net->serverDisconnect();
+	$net->serverConnect( $ip, $port );
+
+	my $msg = $self->reconstruct(
+		{
+			switch         => 'token_login',
+			version        => 55,
+			username       => $username,
+			password       => $password,
+			master_version => 22,
+		}
+	);
+
+	$self->sendToServer( $msg );
+	debug "Sent ReClassic token_login (0064)\n", "sendPacket", 2;
 }
 
 sub sendMapLogin {
@@ -124,13 +133,14 @@ sub sendMapLogin {
 	my $msg;
 	$sex = 0 if ( $sex > 1 || $sex < 0 );    # Sex can only be 0 (female) or 1 (male)
 
+	# ReClassic: map_login = a4 a4 a4 V C (19 bytes), SEM o campo "unknown"
+	# que o ServerType0 padrao usa. Capturado da DLL/cliente.
 	my $msg = $self->reconstruct(
 		{
 			switch    => 'map_login',
 			accountID => $accountID,
 			charID    => $charID,
 			sessionID => $sessionID,
-			unknown   => 4011065369,
 			tick      => getTickCount,
 			sex       => $sex,
 		}
